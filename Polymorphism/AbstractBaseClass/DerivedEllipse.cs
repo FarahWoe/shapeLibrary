efe011d8bd565c50250c804d091140c8c2cf8414
Farah Woe <farah.woelfl@gmail.com>
2021-11-13 12:35:44 +0100

    functions GetArea, GetPerimeter and PrintShapeType added to ellipseClass

--- Polymorphism/AbstractBaseClass/DerivedEllipse.cs
+++ Polymorphism/AbstractBaseClass/DerivedEllipse.cs
@@ -4,7 +4,14 @@ using System.Text;
 
 namespace Bif3.Swe1.Oop.Polymorphism.AbstractBaseClass
 {
-    class Class1
+    class DerivedEllipse : AbstractShape
     {
+        private int _brennpkt1;
+        private int _brennpkt2;
+
+        public DerivedEllipse(int brennpkt1, int brennpkt2, int x, int y) : base(x,y) {
+            this._brennpkt1 = brennpkt1;
+            this._brennpkt2 = brennpkt2;
+            }
     }
 }
